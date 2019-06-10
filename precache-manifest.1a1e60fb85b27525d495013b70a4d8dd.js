4b30d27eebf9f6a6b749cbfa20eff43b127230eb
Brian Kandrac <b_kand@yahoo.com>
2019-06-10 19:10:09 -0400

    Updates

---
+++ precache-manifest.1a1e60fb85b27525d495013b70a4d8dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbbcb88ee975da124841a2fdfeb1025",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "68f01536e4d6fe69acdd",
+    "url": "/ClickyGame/static/css/main.1586082a.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ClickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "68f01536e4d6fe69acdd",
+    "url": "/ClickyGame/static/js/main.9b208903.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
